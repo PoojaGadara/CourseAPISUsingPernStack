43f583b75234fa783177f791ce2483f4e9aec041
unknown <poojagadara@gmail.com>
2024-04-12 10:10:46 +0530

    new commit

--- services/responseMessageService.js
+++
@@ -1,8 +0,0 @@
-class responseMessageService {
-  static async inValidToken() {
-      var msg = { success: false, message: 'Failed to authenticate token.' };
-      return msg;
-  }
-}
-
-export default responseMessageService;
